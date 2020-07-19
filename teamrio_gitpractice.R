bff12ad8e9099e5054f97ab56f68b0e445665b05
heidiaklotz <heidiaklotz@gmail.com>
2020-07-18 22:19:09 -0400

    Vector Element Arithmetic
    Added code for sum, mean & prod.

--- teamrio_gitpractice.R
+++ teamrio_gitpractice.R
@@ -16,3 +16,17 @@ print("Hello World!", quote = FALSE)
 
 # If there are more than 1 item, we can concatenate using paste()
 print(paste("How","are","you?"))
+
+
+## Vector Elements Arithmetic
+sum(2,7,5)
+
+x <- c(2, NA, 3, 1, 4)
+
+sum(x)
+
+sum(x, na.rm=TRUE)    # this way we can ignore NA and NaN values
+
+mean(x, na.rm=TRUE)
+
+prod(x, na.rm=TRUE)
