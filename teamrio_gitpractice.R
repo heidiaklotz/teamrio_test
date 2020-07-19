bcb5a30bb6be57f42335988115b426294f0ea06a
heidiaklotz <heidiaklotz@gmail.com>
2020-07-18 22:17:48 -0400

    Initial Code Set-Up
    The Hello World Example

---
+++ teamrio_gitpractice.R
@@ -0,0 +1,18 @@
+##################################
+## Team Rio - GitHub in R Practice
+##################################
+
+## R Programming Examples
+# https://www.datamentor.io/r-programming/examples/
+
+
+## Hello World Example
+
+# We can use the print() function
+print("Hello World!")
+
+# Quotes can be suppressed in the output
+print("Hello World!", quote = FALSE)
+
+# If there are more than 1 item, we can concatenate using paste()
+print(paste("How","are","you?"))
